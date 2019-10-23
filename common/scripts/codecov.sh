8748e1c0e66e1e77dbf5d07213cfc39b0b29d707
Kuan Feng <31184439+kuanf@users.noreply.github.com>
2019-10-22 22:55:01 -0400

    Cont (#8)
    * dco not cla
    
    Signed-off-by: Kuan Feng <fengkuan@gmail.com>
    
    * ignore dco for members
    
    * fix lint issue

--- common/scripts/codecov.sh
+++ common/scripts/codecov.sh
@@ -148,5 +148,5 @@ fi
 
 # Upload to codecov.io in post submit only for visualization
 if [ "${CODECOV_TOKEN:-}" != "" ]; then
-  @bash <(curl -s https://codecov.io/bash) -t "${CODECOV_TOKEN}" -f "${REPORT_PATH}/coverage.cov"
+  bash <(curl -s https://codecov.io/bash) -t "${CODECOV_TOKEN}" -f "${REPORT_PATH}/coverage.cov"
 fi
