423f2b17289491b82e24ee65d45bf7584f3d555e
Xiangjing Li <xilica.ibm.com@xiangjings-mbp.war.can.ibm.com>
2020-02-20 17:06:55 -0500

    add kubebuilder dependency script lint error
    Signed-off-by: Xiangjing Li <xilica.ibm.com@xiangjings-mbp.war.can.ibm.com>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -14,13 +14,13 @@ if ! [ -x "$(command -v operator-sdk)" ]; then
     sudo mv operator-sdk /usr/local/bin/operator-sdk
 fi
 
-os=$(go env GOOS)
-arch=$(go env GOARCH)
+_OS=$(go env GOOS)
+_ARCH=$(go env GOARCH)
 
 # download kubebuilder and extract it to tmp
-curl -L https://go.kubebuilder.io/dl/2.2.0/${os}/${arch} | tar -xz -C /tmp/
+curl -L https://go.kubebuilder.io/dl/2.2.0/${_OS}/${_ARCH} | tar -xz -C /tmp/
 
 # move to a long-term location and put it on your path
 # (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-sudo mv /tmp/kubebuilder_2.2.0_${os}_${arch} /usr/local/kubebuilder
+sudo mv /tmp/kubebuilder_2.2.0_${_OS}_${_ARCH} /usr/local/kubebuilder
 export PATH=$PATH:/usr/local/kubebuilder/bin
