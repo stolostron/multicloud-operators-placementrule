77ae71a0cfef49b6b0f40a75c32c8e3e649c3f6f
Xiangjing Li <xilica.ibm.com@Xiangjings-MacBook-Pro.local>
2020-02-20 18:02:43 -0500

    Install Kubebuilder components for unit test
    Signed-off-by: Xiangjing Li <xilica.ibm.com@Xiangjings-MacBook-Pro.local>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -13,14 +13,3 @@ if ! [ -x "$(command -v operator-sdk)" ]; then
     chmod +x operator-sdk
     sudo mv operator-sdk /usr/local/bin/operator-sdk
 fi
-
-_OS=$(go env GOOS)
-_ARCH=$(go env GOARCH)
-
-# download kubebuilder and extract it to tmp
-curl -L https://go.kubebuilder.io/dl/2.2.0/"${_OS}"/"${_ARCH}" | tar -xz -C /tmp/
-
-# move to a long-term location and put it on your path
-# (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-sudo mv /tmp/kubebuilder_2.2.0_"${_OS}"_"${_ARCH}" /usr/local/kubebuilder
-export PATH=$PATH:/usr/local/kubebuilder/bin
