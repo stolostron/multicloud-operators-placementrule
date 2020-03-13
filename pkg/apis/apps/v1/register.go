4537c46f9ca5f86e8c392524587db50afa61620c
Yu Cao <ycao@redhat.com>
2020-03-13 15:42:49 -0400

    mroe changes

--- pkg/apis/apps/v1/register.go
+++ pkg/apis/apps/v1/register.go
@@ -30,6 +30,7 @@ var (
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+	AddToScheme   = SchemeBuilder.AddToScheme
 )
 
 // Resource takes an unqualified resource and returns a Group qualified GroupResource
