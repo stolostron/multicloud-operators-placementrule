0cd454392be52ff28ad35bd2807aa62e2af4f451
Mike Ng <ming@redhat.com>
2021-03-16 15:42:00 -0400

    fix prow
    Signed-off-by: Mike Ng <ming@redhat.com>

--- build/run-code-lint.sh
+++ build/run-code-lint.sh
@@ -22,7 +22,7 @@ gem install mdl
 gem install awesome_bot
 
 # Install golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.20.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.24.0
 
 # Start lint task
-make lint
+make -f Makefile.prow lint-all
