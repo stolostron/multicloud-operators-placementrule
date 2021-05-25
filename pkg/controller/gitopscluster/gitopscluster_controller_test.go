9f3d824d7921b5c887eb34755d3cb2c86af846d3
Roke Jung <roke@redhat.com>
2021-05-25 10:02:32 -0400

    placement integration
    Signed-off-by: Roke Jung <roke@redhat.com>

--- pkg/controller/gitopscluster/gitopscluster_controller_test.go
+++ pkg/controller/gitopscluster/gitopscluster_controller_test.go
@@ -589,7 +589,9 @@ func TestReconcileDeleteOrphanSecret(t *testing.T) {
 
 	time.Sleep(time.Second * 3)
 	placementDecision_afterupdate4 := &clusterv1alpha1.PlacementDecision{}
-	g.Expect(c.Get(context.TODO(), types.NamespacedName{Namespace: placementDecision4.Namespace, Name: placementDecision4.Name}, placementDecision_afterupdate4)).NotTo(gomega.HaveOccurred())
+	g.Expect(c.Get(context.TODO(),
+		types.NamespacedName{Namespace: placementDecision4.Namespace,
+			Name: placementDecision4.Name}, placementDecision_afterupdate4)).NotTo(gomega.HaveOccurred())
 
 	g.Expect(placementDecision_afterupdate4.Status.Decisions[0].ClusterName).To(gomega.Equal("cluster1"))
 
